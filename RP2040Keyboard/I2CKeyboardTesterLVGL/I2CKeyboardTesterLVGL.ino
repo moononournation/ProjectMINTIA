148f2f8c8ec7bb21d8fd2687647a9751cc06514a
moononournation <moononournation@gmail.com>
2026-07-31 21:49:22 +0800

    add I2CKeyboardTesterLVGL.ino

---
+++ RP2040Keyboard/I2CKeyboardTesterLVGL/I2CKeyboardTesterLVGL.ino
@@ -0,0 +1,219 @@
+#define I2C_DEV_ADDR 0x55
+#define I2C_SDA 5
+#define I2C_SCL 6
+
+#include <Wire.h>
+
+#define LV_CONF_INCLUDE_SIMPLE
+#include <lvgl.h>
+
+// Dev Device Pins: <https://github.com/moononournation/Dev_Device_Pins.git>
+#include "PINS_T-DISPLAY_S3_PRO.h"
+
+#ifdef CANVAS_PREFERRED
+#define DIRECT_RENDER_MODE // full frame buffer
+#endif
+
+#include "touch.h"
+
+uint32_t screenWidth;
+uint32_t screenHeight;
+uint32_t bufSize;
+lv_display_t *disp;
+lv_color_t *disp_draw_buf;
+
+#if LV_USE_LOG != 0
+void my_print(lv_log_level_t level, const char *buf)
+{
+  LV_UNUSED(level);
+  Serial.println(buf);
+  Serial.flush();
+}
+#endif
+
+uint32_t millis_cb(void)
+{
+  return millis();
+}
+
+/* LVGL calls it when a rendered image needs to copied to the display*/
+void my_disp_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
+{
+#ifndef DIRECT_RENDER_MODE
+  uint32_t w = lv_area_get_width(area);
+  uint32_t h = lv_area_get_height(area);
+
+  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)px_map, w, h);
+#endif // #ifndef DIRECT_RENDER_MODE
+
+  /*Call it to tell LVGL you are ready*/
+  lv_disp_flush_ready(disp);
+}
+
+/*Read the touchpad*/
+void my_touchpad_read(lv_indev_t *indev, lv_indev_data_t *data)
+{
+  if (touch_has_signal())
+  {
+    if (touch_touched())
+    {
+      data->state = LV_INDEV_STATE_PRESSED;
+Serial.printf("%d, %d\n", touch_last_x, touch_last_y);
+      /*Set the coordinates*/
+      data->point.x = touch_last_x;
+      data->point.y = touch_last_y;
+    }
+    else if (touch_released())
+    {
+      data->state = LV_INDEV_STATE_RELEASED;
+    }
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_RELEASED;
+  }
+}
+
+static void my_keyboard_read(lv_indev_t * indev, lv_indev_data_t * data)
+{
+  data->state = LV_INDEV_STATE_RELEASED;
+  Wire.requestFrom(I2C_DEV_ADDR, 1);
+  if (Wire.available() > 0) {
+    while (Wire.available() > 0) {
+      char c = Wire.read();
+      if (c) {
+        data->state = LV_INDEV_STATE_PRESSED;
+        data->key = c;
+        Serial.println(c);
+      }
+    }
+  }
+}
+
+void setup()
+{
+#ifdef DEV_DEVICE_INIT
+  DEV_DEVICE_INIT();
+#endif
+
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+  // while(!Serial);
+  Serial.println("I2C Keyboard Tester");
+  String LVGL_Arduino = String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+  Serial.println(LVGL_Arduino);
+
+  Wire.begin(I2C_SDA, I2C_SCL);
+  Wire.requestFrom(I2C_DEV_ADDR, 1);
+  if (Wire.read() == -1)
+  {
+    Serial.println("I2C Keyboad not online!");
+  }
+
+  // Init Display
+  if (!gfx->begin())
+  {
+    Serial.println("gfx->begin() failed!");
+  }
+  gfx->fillScreen(RGB565_BLACK);
+
+#ifdef GFX_BL
+  pinMode(GFX_BL, OUTPUT);
+  digitalWrite(GFX_BL, HIGH);
+#endif
+
+  // Init touch device
+  touch_init(gfx->width(), gfx->height(), gfx->getRotation());
+
+  lv_init();
+
+  /*Set a tick source so that LVGL will know how much time elapsed. */
+  lv_tick_set_cb(millis_cb);
+
+  /* register print function for debugging */
+#if LV_USE_LOG != 0
+  lv_log_register_print_cb(my_print);
+#endif
+
+  screenWidth = gfx->width();
+  screenHeight = gfx->height();
+
+#ifdef DIRECT_RENDER_MODE
+  bufSize = screenWidth * screenHeight;
+#else
+  bufSize = screenWidth * 40;
+#endif
+
+#ifdef ESP32
+#if defined(DIRECT_RENDER_MODE) && (defined(CANVAS) || defined(RGB_PANEL) || defined(DSI_PANEL))
+  disp_draw_buf = (lv_color_t *)gfx->getFramebuffer();
+#else  // !(defined(DIRECT_RENDER_MODE) && (defined(CANVAS) || defined(RGB_PANEL) || defined(DSI_PANEL)))
+  disp_draw_buf = (lv_color_t *)heap_caps_malloc(bufSize * 2, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+  if (!disp_draw_buf)
+  {
+    // remove MALLOC_CAP_INTERNAL flag try again
+    disp_draw_buf = (lv_color_t *)heap_caps_malloc(bufSize * 2, MALLOC_CAP_8BIT);
+  }
+#endif // !(defined(DIRECT_RENDER_MODE) && (defined(CANVAS) || defined(RGB_PANEL) || defined(DSI_PANEL)))
+#else  // !ESP32
+  Serial.println("LVGL disp_draw_buf heap_caps_malloc failed! malloc again...");
+  disp_draw_buf = (lv_color_t *)malloc(bufSize * 2);
+#endif // !ESP32
+  if (!disp_draw_buf)
+  {
+    Serial.println("LVGL disp_draw_buf allocate failed!");
+  }
+  else
+  {
+    disp = lv_display_create(screenWidth, screenHeight);
+    lv_display_set_flush_cb(disp, my_disp_flush);
+#ifdef DIRECT_RENDER_MODE
+    lv_display_set_buffers(disp, disp_draw_buf, NULL, bufSize * 2, LV_DISPLAY_RENDER_MODE_DIRECT);
+#else
+    lv_display_set_buffers(disp, disp_draw_buf, NULL, bufSize * 2, LV_DISPLAY_RENDER_MODE_PARTIAL);
+#endif
+
+    /*Initialize the (dummy) input device driver*/
+    lv_indev_t *indev = lv_indev_create();
+    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
+    lv_indev_set_read_cb(indev, my_touchpad_read);
+
+    /* Initialize I2C keyboard input device driver */
+    lv_indev_t * indev_key = lv_indev_create();
+    lv_indev_set_type(indev_key, LV_INDEV_TYPE_KEYPAD);
+    lv_indev_set_read_cb(indev_key, my_keyboard_read);
+    lv_group_t * g = lv_group_create();
+    lv_group_set_default(g);
+    lv_indev_set_group(indev_key, g);
+
+    // simple example
+    lv_obj_t *obj = lv_textarea_create(lv_scr_act());
+    lv_obj_set_size(obj, screenWidth, screenHeight);
+    lv_obj_align(obj, LV_ALIGN_CENTER, 0, 0);
+    lv_textarea_set_placeholder_text(obj, "I2C Keyboard Tester");
+    lv_textarea_set_one_line(obj, false);
+    lv_obj_set_style_text_font(obj, &lv_font_montserrat_48, 0);
+    lv_obj_add_state(obj, LV_STATE_FOCUSED);
+  }
+
+  Serial.println("Setup done");
+}
+
+void loop()
+{
+  lv_task_handler(); /* let the GUI do its work */
+
+#ifdef DIRECT_RENDER_MODE
+#if defined(CANVAS) || defined(RGB_PANEL) || defined(DSI_PANEL)
+  gfx->flush();
+#else  // !(defined(CANVAS) || defined(RGB_PANEL) || defined(DSI_PANEL))
+  gfx->draw16bitRGBBitmap(0, 0, (uint16_t *)disp_draw_buf, screenWidth, screenHeight);
+#endif // !(defined(CANVAS) || defined(RGB_PANEL) || defined(DSI_PANEL))
+#else  // !DIRECT_RENDER_MODE
+#ifdef CANVAS
+  gfx->flush();
+#endif
+#endif // !DIRECT_RENDER_MODE
+
+  delay(5);
+}
